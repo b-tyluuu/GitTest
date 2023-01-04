8269b628fec2530064a35cc2281933458dd6fafb
Kutman Kakiev <>
2023-01-05 06:37:31 +0900

    changes

---
+++ GitTestProjekt/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTestProjekt
+//
+//  Created by Kutman Kakiev on 5/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello World")
+        print("git")
+        print("second commit")
+    }
+
+
+}
+
